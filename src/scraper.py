4ea2660f2d1b8c76ccf7b130d43dc1a54b2edf59
tteguayco <teguayco95@gmail.com>
2017-10-09 13:03:53 +0100

    Getting the list of fields for the dataset (variables' names)

--- src/scraper.py
+++ src/scraper.py
@@ -8,10 +8,35 @@ class AccidentsScraper():
 		self.url = "https://aviation-safety.net/database/"
 		self.data = []
 
+	def __download_html(self, url):
+		response = urllib2.urlopen(url)
+		html = response.read()
+		return html
+
+	def __scrape_variables(self, html):
+		bs = BeautifulSoup(html, 'html.parser')
+		td = bs.find('td', class_='list')
+		accident_link = td.next_element.next_element['href']
+
+		# Remove prefix '/database/'
+		accident_link = accident_link.replace('/database/', '')
+		print accident_link
+
+		# Get the target fields
+		fields_str = []
+		html = self.__download_html(self.url + accident_link)
+		bs = BeautifulSoup(html, 'html.parser')
+		fields = bs.findAll('td', class_='caption')
+		for field in fields:
+			# Remove colon and add the name of the variable to the array
+			fields_str.append(field.text.replace(':', ''))
+
+		# Save fields
+		self.data.append(fields_str)
+
 	def scrape(self):
 		# Download HTML
-		response = urllib2.urlopen(self.url)
-		html = response.read()
+		html = self.__download_html(self.url)
 		bs = BeautifulSoup(html, 'html.parser')
 
 		# Get the links for each year
@@ -22,7 +47,12 @@ class AccidentsScraper():
 			if re.match("(19|20)[0-9][0-9]", a.text):
 				years_links.append(a['href'])
 
-		print years_links
+		# Get the headers of the dataset (variables' names)
+		# using just the first href obtained
+		first_link = self.url + years_links[0]
+		print first_link
+		first_link_html = self.__download_html(first_link)
+		self.__scrape_variables(first_link_html)
 
 	def data2csv(self, output_file):
 		pass
